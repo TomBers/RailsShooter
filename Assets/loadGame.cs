3be4e87ea2ec548c402d3821bd031cd1a4a5d8f1
TomBers <tjcberman@gmail.com>
2018-06-08 12:42:05 +0100

    first commit

---
+++ Assets/loadGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadGame : MonoBehaviour {
+
+	private void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadFirstLevel", 2f);
+	}
+
+	private void LoadFirstLevel() {
+		SceneManager.LoadScene(1);
+	}
+}
